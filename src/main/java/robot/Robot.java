c625d5cd80da54186499e96b7f26ea03e3dbc14b
java-marshmallow <aedanlazauskas@gmail.com>
2024-12-09 20:19:37 -0500

    Completed required sections of Differential Drive

--- src/main/java/robot/Robot.java
+++ src/main/java/robot/Robot.java
@@ -19,6 +19,7 @@ import monologue.Logged;
 import monologue.Monologue;
 import org.littletonrobotics.urcl.URCL;
 import robot.Ports.OI;
+import robot.drive.Drive;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -34,6 +35,8 @@ public class Robot extends CommandRobot implements Logged {
   private final PowerDistribution pdh = new PowerDistribution();
 
   // SUBSYSTEMS
+
+  Drive drive = new Drive();
 
   // COMMANDS
 
@@ -70,7 +73,9 @@ public class Robot extends CommandRobot implements Logged {
   }
 
   /** Configures trigger -> command bindings. */
-  private void configureBindings() {}
+  private void configureBindings() {
+    drive.setDefaultCommand(drive.drive(driver::getLeftY, driver::getRightY));
+  }
 
   /**
    * Command factory to make both controllers rumble.
